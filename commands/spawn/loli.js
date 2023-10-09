6e0c1817d712caf563d8d356c77311b268bc2bf3
Adrian Barragan <barraganadrian2001@gmail.com>
2023-10-09 02:06:02 -0700

    Finished update for Loli

--- commands/spawn/loli.js
+++ commands/spawn/loli.js
@@ -13,7 +13,7 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/nDd6Bnz/sothis-fire-emblem-and-1-more-drawn-by-inkanii-ac3f298755b9958d7648cdee838c61e4.jpg')
                 break;
             case 2: 
-                loliName = 'a Loli'
+                loliName = 'Angel'
                 loliImage = ('https://i.ibb.co/gdkNZpt/tenshi-and-yui-angel-beats-drawn-by-goto-p-0f84ec9f69605abc32ecaef1f864404c.png')
                 break;
             case 3: 
@@ -37,7 +37,7 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/Wc1x4QM/2e388ea91b9d7d41312211d070a828951623373035041.jpg')
                 break;
             case 8: 
-                loliName = 'a Loli'
+                loliName = 'Loli'
                 loliImage = ('https://i.ibb.co/jDY1D44/Hatsune-Miku-full-1991271.jpg')
                 break;
             case 9: 
@@ -49,11 +49,11 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/df2XQR3/9e5243e27d8ad232eb844086eab8453a1623373016457.jpg')
                 break;
             case 11: 
-                loliName = 'a Loli'
+                loliName = 'Fischl'
                 loliImage = ('https://i.ibb.co/Hnbbkb4/68b30d8583ecc3fc1017e986afa06adf1623373032182.jpg')
                 break;
             case 12: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/k2yZfHb/70-705133-nao-tomori-charlotte-nao-tomori-fanart.jpg')
                 break;
             case 13: 
@@ -93,7 +93,7 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/cg9tkG3/2055291.jpg')
                 break;
             case 22: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/CJh7BL2/7418490e3ea4ac27c08327c4906bbc21.jpg')
                 break;
             case 23: 
@@ -109,27 +109,27 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/W2C6HxX/76169390-p0-master1200.webp')
                 break;
             case 26: 
-                loliName = 'a Loli'
+                loliName = 'Lil Miku and her friend'
                 loliImage = ('https://i.ibb.co/ZY9TgRj/EWgvj-ZBVc-AA-K-0.jpg')
                 break;
             case 27: 
                 loliName = 'a Loli'
-                loliImage = ('https://i.ibb.co/T4h8P7N/CKXNCIl-UEAALZUk.png')
+                loliImage = ('https://i.ibb.co/xFPxYJ4/1696842103760.jpg')
                 break; 
             case 28: 
-                loliName = 'a Loli'
-                loliImage = ('https://i.ibb.co/bX4gSbt/2-D716216-5-FE1-4-AE9-92-CE-7-CBDF6076-D3-D.jpg')
+                loliName = 'Little Rapunzel'
+                loliImage = ('https://i.ibb.co/HHT2Nxg/1696841925967.jpg')
                 break;
             case 29: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/J7rm3q0/RDT-20220409-1921293356594167491099396.webp')
                 break;
             case 30: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/QMRbYfy/RDT-20220409-1922266282244069323845430.webp')
                 break; 
             case 31: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/2s0GrwW/RDT-20220409-1922167325721227944969689.webp')
                 break;
             case 32: 
@@ -153,7 +153,7 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/gVj99yP/89673134-p1-master1200.webp')
                 break;
             case 37: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/Pww67Kc/RDT-20220409-1921243651344211864971068.webp')
                 break;
             case 38: 
@@ -177,15 +177,15 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/f1RRv7c/90674907-p2-master1200.webp')
                 break;
             case 43: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/zFkmScJ/RDT-20220409-1921153897184685950404601.webp')
                 break;
             case 44: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/MBhzrTT/a1c6c873c2bb28a0a2e7f85669d41412.jpg')
                 break;
             case 45: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/tqLz5qN/RDT-20220409-192110591500794542878943.webp')
                 break;
             case 46: 
@@ -197,23 +197,23 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/cJXQxT5/Adele-von-Ascham-600-2733860.jpg')
                 break;
             case 48: 
-                loliName = 'a Loli'
+                loliName = 'Miku'
                 loliImage = ('https://i.ibb.co/r5pNdSJ/ag13.jpg')
                 break;
             case 49: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/HB67xyJ/RDT-20220409-1921053948255126203690264.webp')
                 break;
             case 50: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/4ZT0kD3/RDT-20220409-1920207470724832719424644.webp')
                 break;
             case 51: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/cyD3TbV/anime-image-f601936a02-png.jpg')
                 break;
             case 52: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/smPGQ4g/RDT-20220409-1920019029176334513146996.webp')
                 break;
             case 53: 
@@ -221,15 +221,15 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/3NMhHym/bb7b3c374bfe13ad88b3947a069cc5211623372984771.jpg')
                 break;
             case 54: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/M2KWGWj/RDT-20220409-1919567935644390160181082.webp')
                 break;
             case 55: 
-                loliName = 'a Loli'
+                loliName = 'a Loli, pat pat'
                 loliImage = ('https://i.ibb.co/JHvYc9S/Bkj-Nuq-d-W.png')
                 break;
             case 56: 
-                loliName = 'a Loli'
+                loliName = 'a free trip to jail, hop in!'
                 loliImage = ('https://i.ibb.co/JpnNqfG/Bowed-Ornery-Goa-small.gif')
                 break;
             case 57: 
@@ -237,35 +237,35 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/xhSVy0G/By-Srd8-QD.gif')
                 break;
             case 58: 
-                loliName = 'a Loli'
+                loliName = 'Char, "say Cheese"'
                 loliImage = ('https://i.ibb.co/t83dBhk/RDT-20220409-1919492058173941851250103.webp')
                 break;
             case 59: 
-                loliName = 'a Loli'
+                loliName = 'Fischl'
                 loliImage = ('https://i.ibb.co/5h3NLy1/cb5f6434a671b89a46a945a555b0e08b-6826145892292585740.jpg')
                 break;
             case 60: 
-                loliName = 'a Loli'
+                loliName = 'our lovely daughters❤️'
                 loliImage = ('https://i.ibb.co/MpyD0P2/RDT-20220409-191934470549225645300602.webp')
                 break;
             case 61: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/BB5PcpC/RDT-20220409-1918471010894379589564608.webp')
                 break;
             case 62: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/6FWNgGq/RDT-20220409-1918433180122379057171545.webp')
                 break;
             case 63: 
-                loliName = 'a Loli'
+                loliName = 'Ashley'
                 loliImage = ('https://i.ibb.co/L6f8tmZ/cg16-3.jpg')
                 break;
             case 64: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/hsxCFx3/RDT-20220409-1917464176846198563961056.webp')
                 break;
             case 65: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/F5L9mFn/Charlotte-05-Large-11.jpg')
                 break;
             case 66: 
@@ -285,7 +285,7 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/y4dqp6z/dc0.png')
                 break;
             case 70: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/BL7b9jg/RDT-20220409-1916368294803177869502292.webp')
                 break;
             case 71: 
@@ -297,7 +297,7 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/ZNxfYFR/e81d1b1199d05321ca84fd8f1b63b3c01623373021586.jpg')
                 break;
             case 73: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/RNYrbRt/RDT-20220409-1916163422353743644953529.webp')
                 break;
             case 74: 
@@ -309,11 +309,11 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/d2wGLKd/ee35350a0cb5ff0731e0100c5e925b001623276535908.jpg')
                 break;
             case 76: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/hDkpBHb/72wo0p5fhaj41.png')
                 break;
             case 77: 
-                loliName = 'a Loli'
+                loliName = 'Maid Char!'
                 loliImage = ('https://i.ibb.co/kykC8Dv/8664705bfedab06e33e98fe32f5c6e98-remastered.jpg')
                 break;
             case 78: 
@@ -326,18 +326,18 @@ module.exports = {
                 break;
             case 80: 
                 loliName = 'a Loli'
-                loliImage = ('https://i.ibb.co/sRBdxRc/fg15.jpg')
+                loliImage = ('https://i.ibb.co/BctRj7n/1696841421411.jpg')
                 break;
             case 81: 
-                loliName = 'a Loli'
+                loliName = 'Fischl'
                 loliImage = ('https://i.ibb.co/z2TmZ7k/Fischl-full-3113664.jpg')
                 break;
             case 82: 
-                loliName = 'a Loli'
+                loliName = 'Fischl'
                 loliImage = ('https://i.ibb.co/YtFMjGq/fischl-from-genshin-impact-by-yayachann-de6yzjg-fullview.jpg')
                 break;
             case 83: 
-                loliName = 'a Loli'
+                loliName = 'Mom\'s fav'
                 loliImage = ('https://i.ibb.co/SJDHwbG/g1.jpg')
                 break;
             case 84: 
@@ -349,15 +349,15 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/XzpGr4Y/Hkv-Y8-JKPW.gif')
                 break;
             case 86: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/d07hYPB/4f014389f01eb2a6f38907b0bff12478-remastered.jpg')
                 break;
             case 87: 
                 loliName = 'a Loli'
-                loliImage = ('https://i.ibb.co/kQT9bdJ/image0-1.jpg')
+                loliImage = ('https://i.ibb.co/980LtXt/1696841234530.jpg')
                 break;
             case 88: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/XFdN5wt/7418490e3ea4ac27c08327c4906bbc21.jpg')
                 break;
             case 89: 
@@ -370,26 +370,26 @@ module.exports = {
                 break;
             case 91: 
                 loliName = 'a Loli'
-                loliImage = ('https://i.ibb.co/NWWvNhJ/image0-15.jpg')
+                loliImage = ('https://i.ibb.co/ZWG6qny/1696841125604.jpg')
                 break;
             case 92: 
                 loliName = 'a Loli'
                 loliImage = ('https://i.ibb.co/qk6nmxB/image0-19-1-1.jpg')
                 break;
             case 93: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/gD47f4b/RDT-20220409-1922354284478178336484564.webp')
                 break;
             case 94: 
-                loliName = 'a Loli'
+                loliName = 'Mom\'s fav loli'
                 loliImage = ('https://i.ibb.co/MhTjtJJ/4ac162c08c2024e48b41aa45ed3df618.jpg')
                 break;
             case 95: 
-                loliName = 'a Loli'
-                loliImage = ('https://i.ibb.co/Syw44dt/ffe23f9060f31374a963fbfdf6cbb425.jpg')
+                loliName = 'Mom\'s fav loli'
+                loliImage = ('https://i.ibb.co/TckSFQz/1696840947553.jpg')
                 break;
             case 96: 
-                loliName = 'a Loli'
+                loliName = 'a free trip to jail!'
                 loliImage = ('https://i.ibb.co/Bnd8Bhz/k9hubx7dfaq01.jpg')
                 break;
             case 97: 
@@ -409,23 +409,23 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/23gsGzb/Mona.jpg')
                 break;
             case 102: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/RBR471X/Nao-Tomori-Anime-Manga-Desktop-Anime-PNG-clipart-free.jpg')
                 break;
             case 103: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/9vb9qkn/Nao-Tomori-charlotte-anime-39361164-620-876.jpg')
                 break;
             case 104: 
-                loliName = 'a Loli'
+                loliName = 'Mom\'s fav Loli'
                 loliImage = ('https://i.ibb.co/ckmx1ww/6333926891ec74e5337ff3f548c615b1.jpg')
                 break;
             case 105: 
-                loliName = 'a Loli'
+                loliName = 'Mom\'s fav Loli'
                 loliImage = ('https://i.ibb.co/VHLqHyL/d8079a931d73a9b144b95c73fec0dbcf.jpg')
                 break;
             case 106: 
-                loliName = 'a Loli'
+                loliName = 'Mom\'s fav Loli'
                 loliImage = ('https://i.ibb.co/q0PFLt4/4cd3e099fa60bf88ae45c503cadf584b.jpg')
                 break;
             case 107: 
@@ -441,19 +441,19 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/VMF99Dj/d303fe1f5fb54ec19473540bef826fb2.jpg')
                 break;
             case 110: 
-                loliName = 'a Loli'
+                loliName = 'Char'
                 loliImage = ('https://i.ibb.co/bQ3sMQ5/Pics-Art-08-26-09-53-00.jpg')
                 break;
             case 111: 
-                loliName = 'a Loli'
+                loliName = 'Angel'
                 loliImage = ('https://i.ibb.co/gTGKmtP/Pics-Art-08-26-09-53-39.jpg')
                 break;
             case 112: 
-                loliName = 'a Loli'
+                loliName = 'a trap, it\'s dad'
                 loliImage = ('https://i.ibb.co/HV6XsFQ/Pics-Art-09-06-09-14-45.png')
                 break;
             case 113: 
-                loliName = 'a Loli'
+                loliName = 'Angel cosplaying as her favorite sis'
                 loliImage = ('https://i.ibb.co/ygdw2VM/qlqo.jpg')
                 break;
             case 114: 
@@ -485,7 +485,7 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/ZL75bDH/Screenshot-20200819-190426-Azur-Lane.jpg')
                 break;
             case 121: 
-                loliName = 'a Loli'
+                loliName = 'a promise of a lifetime💙'
                 loliImage = ('https://i.ibb.co/pWcP364/Screenshot-20200820-165641-You-Tube.jpg')
                 break;
             case 122: 
@@ -529,11 +529,11 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/zHmKZvw/Screenshot-20201217-101409-Azur-Lane.jpg')
                 break;
             case 132: 
-                loliName = 'a Loli'
+                loliName = 'two Christmas Lolis'
                 loliImage = ('https://i.ibb.co/rmH7j6D/Screenshot-20201217-101415-Azur-Lane.jpg')
                 break;
             case 133: 
-                loliName = 'a Loli'
+                loliName = 'a Catgirl Loli'
                 loliImage = ('https://i.ibb.co/6Z6CTdy/SJNn-8mw.gif')
                 break;
             case 134: 
@@ -545,11 +545,11 @@ module.exports = {
                 loliImage = ('https://i.ibb.co/XSmsP7h/78ee71db3f08e953f73f70fd9ed2d0f3.jpg')
                 break;
             case 136: 
-                loliName = 'a Loli'
+                loliName = 'an emoji'
                 loliImage = ('https://i.ibb.co/TrbynV0/20e86b8e2e15b111046054e077a2ec30.jpg')
                 break;
             case 137: 
-                loliName = 'a Loli'
+                loliName = 'a Angel'
                 loliImage = ('https://i.ibb.co/WKcsv82/thumb-1920-456957.jpg')
                 break;
             case 138: 
