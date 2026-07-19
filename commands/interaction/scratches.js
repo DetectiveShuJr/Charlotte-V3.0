7348aed4dbe39f560008ab7321a5cbbcb8f58d52
Adrian Barragan <barraganadrian2001@gmail.com>
2026-07-18 20:29:38 -0700

    updating Scratches code

--- commands/interaction/scratches.js
+++ commands/interaction/scratches.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const scratch = require('./scratches.json');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -13,33 +14,27 @@ module.exports = {
   async execute(interaction) {
     const targetUser = interaction.options.getUser('target') || interaction.user;
     const author = interaction.user;
+    
 
-    const scratchGifs = [
-      'https://i.ibb.co/4Z6sxHtJ/love-live-anime.gif',
-      'https://i.ibb.co/ksxf3Dpx/chocola-anime.gif',
-      'https://i.ibb.co/BVbMkpR8/skritches-scritches.gif',
-      'https://i.ibb.co/fR2YXp3/anime-cat-girl-anime.gif',
-      'https://i.ibb.co/HpDsvpY4/anime-acchi-kocchi.gif',
-    ];
-
-    const randomGif = scratchGifs[Math.floor(Math.random() * scratchGifs.length)];
+    const randomIndex = Math.floor(Math.random() * scratch.length);
+    const { image: scratchImage, number: scratchNumber } = scratch[randomIndex];
 
     const scratchEmbed = new EmbedBuilder()
       .setColor('#E0FBFF')
-      .setImage(randomGif);
+      .setImage(scratchImage);
 
     if (targetUser.id === author.id) {
       scratchEmbed
         .setAuthor({ name: `${author.username} just scratched themselves..? weirdo`, iconURL: author.displayAvatarURL() })
-        .setFooter({ text: `${author.username} just scratched themselves. Ouch!`, iconURL: author.displayAvatarURL() });
+        .setFooter({ text: `${author.username} just scratched themselves\n${scratchNumber}`, iconURL: author.displayAvatarURL() });
     } else if (targetUser.id === interaction.client.user.id) {
       scratchEmbed
         .setAuthor({ name: `That feels nice... ${author.username}, It's relaxing`, iconURL: author.displayAvatarURL() })
-        .setFooter({ text: `I wouldn't mind some scratches once in a while...`, iconURL: author.displayAvatarURL() });
+        .setFooter({ text: `I wouldn't mind some scratches once in a while...\n${scratchNumber}`, iconURL: author.displayAvatarURL() });
     } else {
       scratchEmbed
         .setAuthor({ name: `${author.username} decided to scratch ${targetUser.username}, they look like they enjoyed it`, iconURL: author.displayAvatarURL() })
-        .setFooter({ text: `${author.username} just scratched ${targetUser.username}. They seem happy!`, iconURL: targetUser.displayAvatarURL() });
+        .setFooter({ text: `${author.username} just scratched ${targetUser.username}. They seem happy!\n${scratchNumber}`, iconURL: targetUser.displayAvatarURL() });
     }
 
     await interaction.reply({ embeds: [scratchEmbed] });
